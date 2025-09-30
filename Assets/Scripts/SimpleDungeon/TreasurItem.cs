cc7253ea4d320de84a03c4d1f2dfb15f14356ba1
ahyun04 <ramaeil125@gmail.com>
2025-09-30 12:23:37 +0900

    [Feat] 자료구조 랜덤 던전 생성 방 만들기

---
+++ Assets/Scripts/SimpleDungeon/TreasurItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasurItem : MonoBehaviour
+{
+    public int goldAmount = 100;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log($"º¸¹° È¹µæ ! °ñµå + {goldAmount}");
+            Destroy(gameObject);
+        }
+    }
+    
+}
